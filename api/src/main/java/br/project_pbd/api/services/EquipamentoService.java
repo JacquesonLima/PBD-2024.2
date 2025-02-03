06955dd2cb4319df5ea89293abe3f9370ece8ae6
JacquesonLima <jacquesonbenevides@gmail.com>
2025-02-03 18:43:42 -0300

    resolução de conflitos

--- api/src/main/java/br/project_pbd/api/services/EquipamentoService.java
+++ api/src/main/java/br/project_pbd/api/services/EquipamentoService.java
@@ -14,13 +14,10 @@ public class EquipamentoService {
   @Autowired
   private EquipamentoRepository equipamentoRepository;
 
-<<<<<<< HEAD
   public EquipamentoService(EquipamentoRepository equipamentoRepository) {
     this.equipamentoRepository = equipamentoRepository;
   }
 
-=======
->>>>>>> 15e1c61806be4c8c3386d8fb7b3ee7791a413c3b
   public List<Equipamento> listarTodos() {
     return equipamentoRepository.findAll();
   }
